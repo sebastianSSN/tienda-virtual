ca024b7934e9f308719ca952d47d2714d7caf82b
Luis Estupinan <lestupinan672@unab.edu.co>
2024-03-05 20:25:53 -0500

    trabajo en clase 05-03-2024

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\productController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/detail', function () {
+    return view('detail');
+});
+
+Route::get('/products', [productController::class, 'index']);
