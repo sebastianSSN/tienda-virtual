e01a6f9f5ef348565b0b3cae7b9c053c73890e36
Luis Estupinan <lestupinan672@unab.edu.co>
2024-03-12 20:18:50 -0500

    trabajo en clase 12/03/24

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomFloat(2,10000,1000000),
+            'url_imgage' => $this->faker->image(null,640,480),
+            'state' => 1
+        ];
+    }
+}
---
+++ database/migrations/2024_03_13_003023_create_product_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->default('');
+            $table->text('description')->default('');
+            $table->double('price')->default(0);
+            $table->string('url_imgage')->default('');
+            $table->integer('state')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
